fd4dce3b7b12ee8d01ad82deab0a52784ac16dff
cobish <cobish110@gmail.com>
2016-07-31 15:16:19 +0800

    commit

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ var CONFIG = {
     repo         : 'cobish.github.io',
     duoshuo      : 'cobish',
     baiduAnaly   : '84602bc3713ca2aeb0258c55adf6a333',
-    access_token : '04409eefe9e1a3842f614e560470c73a0147c9c6'
+    access_token : '6117ebab6cc1cffee7f8d82191c8c937714e05ea'
 };
 
 var duoshuoQuery = {short_name: CONFIG['duoshuo']};
