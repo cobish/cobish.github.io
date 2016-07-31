cd9b23f30abb80d83bda84b7b1dde911e2fac2d9
cobish <cobish110@gmail.com>
2016-07-31 13:11:57 +0800

    fix bug

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ var CONFIG = {
     repo         : 'cobish.github.io',
     duoshuo      : 'cobish',
     baiduAnaly   : '84602bc3713ca2aeb0258c55adf6a333',
-    access_token : 'f5f6af96ab39385b69f67531c5e2b4790808a404'
+    access_token : '39cad2159ccf275e9723826333e203ff4368b68e'
 };
 
 var duoshuoQuery = {short_name: CONFIG['duoshuo']};
