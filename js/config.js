78b4512845dadc9d763358ae40334ddebd36b68f
cobish <cobish110@gmail.com>
2016-09-09 17:10:02 +0800

    删除一些代码

--- js/config.js
+++
@@ -1,9 +0,0 @@
-var CONFIG = {
-    owner        : 'cobish',
-    repo         : 'cobish.github.io',
-    duoshuo      : 'cobish',
-    baiduAnaly   : '84602bc3713ca2aeb0258c55adf6a333',
-    // access_token : '6117ebab6cc1cffee7f8d82191c8c937714e05ea'
-};
-
-var duoshuoQuery = {short_name: CONFIG['duoshuo']};
