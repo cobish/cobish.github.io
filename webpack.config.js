9f2d5a5f1b63e7aa4de7435cb139c5986b518971
cobish <cobish110@gmail.com>
2016-09-12 17:29:56 +0800

    让windows也能跑命令

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,26 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-// var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var OpenBrowserPlugin = require('open-browser-webpack-plugin');
 var WebpackMd5Hash = require('webpack-md5-hash');
+// var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+var copy = require('quickly-copy-file');
+var del = require('del');
 
 // 开发环境
 var isDev = function() {
-  return process.env.NODE_ENV === 'development';
+  return process.env.NODE_ENV.trim() === 'development';
 };
 
 // 生产环境
 var isProd = function() {
-  return process.env.NODE_ENV === 'production';
+  return process.env.NODE_ENV.trim() === 'production';
 };
 
-var plugins = [
-  new webpack.DefinePlugin({
-    __DEV__ : isDev(),
-    __PROD__: isProd(),
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-  }),
-  new webpack.optimize.CommonsChunkPlugin('vendor', isProd() ? 'vendor.[chunkhash:8].js' : 'vendor.js'),
-  // new ExtractTextPlugin(isProd() ? '[name].[chunkhash:8].css' : '[name].css'),
-  new HtmlWebpackPlugin({
-    title: 'cobish - 写给未来的自己',
-    filename: '../index.html',
-    template: './src/html/index.html'
-  })
-];
-
-if (isDev()) {
-  plugins.push(
-    new OpenBrowserPlugin({ url: 'http://localhost:8080/src/html/index_dev.html' })
-  );
-}
-
-if (isProd()) {
-  plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      output: {
-        comments: false,
-      },
-      compress: {
-        warnings: false
-      }
-    }),
-    new WebpackMd5Hash()
-  );
-}
+copyAndDelFiles();
 
 module.exports = {
-  devtool: isProd() ? null : 'inline-source-map',
+  devtool: isProd() ? false : 'inline-source-map',
   entry: {
     index: [
       './src/js/index.js'
@@ -66,8 +35,7 @@ module.exports = {
       'nprogress',
       'jquery',
       'marked',
-      'highlight.js',
-      // 'isomorphic-fetch',
+      'highlight.js'
     ]
   },
   output: {
@@ -91,9 +59,70 @@ module.exports = {
       loaders: ['react-hot', 'babel?presets[]=react,presets[]=es2015']
     }]
   },
-  plugins: plugins
+  plugins: getPlugins()
 };
 
+// 复制和删除文件
+function copyAndDelFiles() {
+  var copyFile = '';
 
+  // 复制文件
+  if (isDev()) {
+    copyFile = 'src/html/index_dev.html';
+  } 
 
+  if (isProd()) {
+    copyFile = 'src/html/index.html';
+  }
 
+  copy(copyFile, 'index.html', function(error) {
+    if (error) {
+      return console.error(error);
+    }
+  });
+
+  if (isProd()) {
+    del(['dist']);
+  }
+}
+
+// 获取配置
+function getPlugins() {
+  var plugins = [
+    new webpack.DefinePlugin({
+      __DEV__ : isDev(),
+      __PROD__: isProd(),
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV.trim())
+    }),
+    new webpack.optimize.CommonsChunkPlugin('vendor', isProd() ? 'vendor.[chunkhash:8].js' : 'vendor.js'),
+    // new ExtractTextPlugin(isProd() ? '[name].[chunkhash:8].css' : '[name].css'),
+  ];
+
+  if (isDev()) {
+    plugins.push(
+      new OpenBrowserPlugin({ url: 'http://localhost:8080/' })
+    );
+  }
+
+  if (isProd()) {
+    plugins.push(
+      new webpack.optimize.UglifyJsPlugin({
+        minimize: true,
+        output: {
+          comments: false,
+        },
+        compress: {
+          warnings: false
+        }
+      }),
+      new HtmlWebpackPlugin({
+        title: 'cobish - 写给未来的自己',
+        filename: '../index.html',
+        template: './src/html/index.html'
+      }),
+      new WebpackMd5Hash()
+    );
+  }
+
+  return plugins
+}
