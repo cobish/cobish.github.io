0fa2d0c6eb1d23ba5acc5920d5f884be2cb18faa
cobish <cobish110@gmail.com>
2016-11-08 22:07:38 +0800

    让微信浏览器的document.title不变

--- src/js/containers/Root.js
+++ src/js/containers/Root.js
@@ -13,6 +13,21 @@ import '../../css/index.scss';
 import '../../css/list.scss';
 import '../../css/nprogress.scss';
 
+//以下代码可以解决以上问题，不依赖jq
+setTimeout(function(){
+  //利用iframe的onload事件刷新页面
+  document.title = CONFIG.title;
+  var iframe = document.createElement('iframe');
+  iframe.style.visibility = 'hidden';
+  iframe.style.width = '1px';
+  iframe.style.height = '1px';
+  iframe.onload = function () {
+    setTimeout(function () {
+      document.body.removeChild(iframe);
+    }, 0);
+  };
+  document.body.appendChild(iframe);
+},0);
 
 const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
 
