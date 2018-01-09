3342d33555f2c779af675af7f3152038899777c0
cobish <cobish110@gmail.com>
2018-01-09 11:33:20 +0800

    babel-preset-es2015 替换成 babel-preset-env

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ module.exports = {
     }, {
       test: /\.js$/,
       exclude: /node_modules/,
-      loaders: ['react-hot', 'babel?presets[]=react,presets[]=es2015']
+      loaders: ['react-hot', 'babel?presets[]=react,presets[]=env']
     }]
   },
   plugins: getPlugins()
