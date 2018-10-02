6267617c4e04e299f83437cafdce89b2e7f91f69
cobish <cobish110@gmail.com>
2018-10-02 10:40:04 +0800

    try access_token

--- src/js/constants/Config.js
+++ src/js/constants/Config.js
@@ -5,5 +5,5 @@ export const CONFIG = {
   title      : 'cobish - 写给未来的自己',
   titleLoad  : 'cobish - 正在加载...',
   baiduAnaly : '84602bc3713ca2aeb0258c55adf6a333',
-  token : '187fe22781ab9c1edba1a73ceec017b57e869cff'
+  token : '5f6d25c2baaadf2122c8dd4190f4979d1664a021'
 };
